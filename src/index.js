ca32152856c0f3433e7eb80d4e072b9a0ad6f35b
TLAMHutto <keatonhutto8@gmail.com>
2022-08-01 22:57:32 -0500

    about button

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@
 fetch('https://www.codewars.com/api/v1/users/TLAMHutto')
     .then(response => response.json())
     .then(data => {
-        console.log(data.ranks.overall);
         let score = data.ranks.overall.score;
+        let rank = data.ranks.overall.rank;
+        let name = data.ranks.overall.name
+        document.getElementById('codeWars').innerHTML = 'Score: ' + score + ' Rank: ' + rank + ' Name: ' + name;
+        console.log(data.ranks.overall);
+        
             
 
     }
